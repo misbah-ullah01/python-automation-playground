3a4a16182836e06d770ccae1edc5fb884d25c29a
Misbah Ullah <misbahu094@gmail.com>
2026-05-25 20:17:54 +0500

    Added .gitignore \ Modified file_renamer to accept paths from user

--- file_renamer.py
+++ file_renamer.py
@@ -1,16 +1,21 @@
 import os
-folder_path = "test_files"
-files = os.listdir(folder_path)
+from pathlib import Path
+folder_path = input("Please enter Directory Name: ")
+path = Path(folder_path)
+if path.is_dir():
+    files = os.listdir(folder_path)
 
-files.sort()
-count = 1
+    files.sort()
+    count = 1
 
-for file_name in files:
-    old_path = os.path.join(folder_path, file_name)
-    extension = os.path.splitext(file_name)[1]
-    new_name = f"vacation_{count:03d}{extension}"
-    new_path = os.path.join(folder_path, new_name)
-    os.rename(old_path, new_path)
+    for file_name in files:
+        old_path = os.path.join(folder_path, file_name)
+        extension = os.path.splitext(file_name)[1]
+        new_name = f"evid_{count:03d}{extension}"
+        new_path = os.path.join(folder_path, new_name)
+        os.rename(old_path, new_path)
 
-    print(f"Renamed: {file_name} -> {new_name}")
-    count += 1
+        print(f"Renamed: {file_name} -> {new_name}")
+        count += 1
+else:
+    print("Path error...")
